2857fc1e7b886248f475fb27c55c84b7d6a3102e
Frank Potthast Rutz <frank.potthast@gmail.com>
2020-04-11 14:16:59 +0200

    added build.sh and respective files

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+
+for PAGE in index stiftungszweck spenden stiftungsrat kontakt
+do
+	echo $PAGE
+	TARGET=$PAGE".html"
+	echo "TARGE:$TARGET"
+	SOURCE="${PAGE}_source.html"
+	echo "SOURCE;$SOURCE"
+
+	cat top.html    >  ./website/${TARGET}
+	cat ${SOURCE}   >> ./website/${TARGET}
+        cat bottom.html >> ./website/${TARGET}
+	cp neuropath.css   ./website/
+	cp w3mobile.css    ./website/
+	cp neuropath.png   ./website/
+done
