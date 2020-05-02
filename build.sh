a1ff42573dead55289b594eb4a790da13f86e816
Frank Potthast Rutz <frank.potthast@gmail.com>
2020-05-02 09:19:18 +0200

    graphical improvements.

--- build.sh
+++ build.sh
@@ -31,6 +31,8 @@ do
 		cp AdrianoAguzzi.jpg        ${TARGETDIR}
 		cp laser.png                ${TARGETDIR}
 		cp protein.png              ${TARGETDIR}
+		cp donate_background.png    ${TARGETDIR}
+		cp contact.png              ${TARGETDIR}
 
        #make sure reference is to correct PAGE
 	   sed -i "s|REPLACE_ME_WITH_PAGE_IN_BUILD|${PAGE}|g" ${TARGETDIR}*.html
